b4c36a8256f7c402320ef93fbebcfbf9e741df6d
Neiman Abou Nasr <nabounasr@gmail.com>
2021-03-31 11:13:08 -0400

    The instructions page was added to the project

--- src/pages/instructions.js
+++ src/pages/instructions.js
@@ -1,25 +1,87 @@
-import logo from "../logo.svg"
-import React from "react"
- 
-function Instructions() {
-    return (
-        <div id="home_page">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                   Welcome to Instruction!
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    )
+import React, {Component} from "react"
+import { Container, Row, Col } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../instructions.css'
+import { withRouter } from "react-router";
+
+class Instructions extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            cocktail: {},
+            ingredients:[],
+            id:  this.props.match.params.id
+        }
+    }
+
+    componentDidMount = () =>{
+       this.loadCocktail();
+        console.log(this.state.cocktail);
+    }
+
+    loadCocktail() {
+        this.setState({ loading: true })
+        console.log("Loading contact data")
+        fetch(`https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=${this.state.id}`)
+        .then(data => data.json())
+        .then(data => this.setState(
+            { cocktail: data.drinks[0],
+            ingredients:[
+                data.drinks[0].strIngredient1,
+                data.drinks[0].strIngredient2,
+                data.drinks[0].strIngredient3,        
+                data.drinks[0].strIngredient4,
+                data.drinks[0].strIngredient5,
+                data.drinks[0].strIngredient6,                     
+                data.drinks[0].strIngredient7,
+                data.drinks[0].strIngredient8,
+                data.drinks[0].strIngredient9,                     
+                data.drinks[0].strIngredient10,
+                data.drinks[0].strIngredient11,
+                data.drinks[0].strIngredient12,                     
+                data.drinks[0].strIngredient13,
+                data.drinks[0].strIngredient14,
+                data.drinks[0].strIngredient15        
+            ],
+            }
+        ))
+    }    
+
+    render() {
+
+
+        return (
+            <Container fluid>
+                <Row className="justify-content-center">
+
+                    <Col md={3} className="mt-5"><img className="img-fluid" src={this.state.cocktail.strDrinkThumb}/></Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col md={3} className="mt-5"><label>Name: </label> {this.state.cocktail.strDrink}</Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col md={3} className="mt-5"><label>Category: </label> {this.state.cocktail.strCategory}</Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col md={2} className="mt-5"><label>Ingredients:</label>
+
+                        <ul>
+                        {this.state.ingredients.map((i, index) => {
+                            if(i){
+                                return <li key={index} className="text-left">{i}</li>
+                            }
+                        })}
+                            
+                        </ul>
+                    </Col>
+                </Row>
+                <Row className="mb-5 justify-content-center">
+                    <Col md={3} className="mt-5"><label>Instructions: </label><br/> {this.state.cocktail.strInstructions}</Col>
+                </Row>
+                
+            </Container>
+        );
+    }
 }
- 
-export default Instructions
+export default withRouter(Instructions)
