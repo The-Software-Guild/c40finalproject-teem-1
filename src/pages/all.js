ee65dd5590e19221668ff31ad738115a92cc9e61
Amkue <Amanda.m.kue@gmail.com>
2021-04-05 11:21:53 -0500

    get terms from API, made suggestions scrollable

--- src/pages/all.js
+++ src/pages/all.js
@@ -56,10 +56,10 @@ class allPage extends Component {
 
         if (!this.props.match.params.element && (this.state.drinks === undefined || this.state.drinks.length == 0)) {
             this.loadAllDrinks()
+            this.getAllSearchTerms()
         }
         else {
             this.loadDrinksFromURL();
-
         }
     }
 
@@ -106,14 +106,57 @@ class allPage extends Component {
             this.setState({drinks: data.drinks})
           })
     }
+
+    //For Autocomplete, get list of terms user can use to search for "category", "glass type", and "alcoholic"
+    getAllSearchTerms = (event) => {
+        let terms = []
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
+            .then(data => data.json())
+            .then(data => {
+                console.log(data.drinks)
+                data.drinks.forEach(drink => {
+                    terms = terms.concat(drink.strCategory)
+                    console.log(terms)
+                })
+                this.setState({ allTerms: terms})
+
+            }) 
+
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list")
+            .then(data => data.json())
+            .then(data => {
+                console.log(data.drinks)
+                data.drinks.forEach(drink => {
+                    terms = terms.concat(drink.strGlass)
+                    console.log(terms)
+                })
+                this.setState({ allTerms: terms})
+
+            }) 
+
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list")
+            .then(data => data.json())
+            .then(data => {
+                console.log(data.drinks)
+                data.drinks.forEach(drink => {
+                    terms = terms.concat(drink.strAlcoholic)
+                    console.log(terms)
+                })
+                this.setState({ allTerms: terms})
+
+            }) 
+        
+        
+    }
     
     //For Autocomplete
     onTextChange = (event) => {
         const value = event.target.value
         let suggestions = [];
+        console.log(this.state.allTerms)
         if (value.length > 0) {
             const regex = new RegExp(`^${value}`, 'i');
-            suggestions = this.allSearchTerms.sort().filter(v => regex.test(v));
+            suggestions = this.state.allTerms.sort().filter(v => regex.test(v));
         }
         this.setState(() => ({ suggestions, searchTerm: value }))  
     } 
@@ -154,22 +197,20 @@ class allPage extends Component {
             .then(data => data.json())
             .then(data => {
                 console.log(data.drinks)
-                    this.setState({ filteredDrinks: data.drinks, drinks: [] }, () => {
-                        console.log(`${this.state.filteredDrinks}`);
-                        this.state.filteredDrinks.forEach(drink => {
-                            console.log('drink : ' + drink.strDrink)
-                            fetch("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drink.idDrink)
-                                .then(data => data.json())
-                                .then(data => {
-                                    this.setState({
-                                        drinks: this.state.drinks.concat(data.drinks)
-                                      })
-                                }) 
-                        })
+                this.setState({ filteredDrinks: data.drinks, drinks: [] }, () => {
+                    console.log(`${this.state.filteredDrinks}`);
+                    this.state.filteredDrinks.forEach(drink => {
+                        console.log('drink : ' + drink.strDrink)
+                        fetch("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drink.idDrink)
+                            .then(data => data.json())
+                            .then(data => {
+                                this.setState({
+                                    drinks: this.state.drinks.concat(data.drinks)
+                                    })
+                            }) 
+                    })
                 });
-            })
-
-            
+            })  
         }
     }
 
