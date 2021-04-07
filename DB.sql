946ee4e505ed492e7b334739a7f721dad92dd201
Neiman Abou Nasr <nabounasr@gmail.com>
2021-04-07 15:44:09 -0400

    Second implementation of the db

--- DB.sql
+++ DB.sql
@@ -10,96 +10,29 @@ CREATE TABLE User(
     password VARCHAR(200) NOT NULL
 );
 
-CREATE TABLE Cocktail(
-	idCocktail INT PRIMARY KEY AUTO_INCREMENT,
-    dateCreated DATE NOT NULL,
-    strAlcoholic VARCHAR(20) NOT NULL,
-    strCategory VARCHAR(40) NOT NULL,
-    name VARCHAR(40) NOT NULL,
-    strInstructions TEXT NOT NULL,
-	idUser INT NOT NULL,
-    FOREIGN KEY FKUser (idUser) REFERENCES User(id)
-);
-
-
-
-CREATE TABLE Measure (
-	idMeasure INT PRIMARY KEY AUTO_INCREMENT,
-    unit VARCHAR(40) NOT NULL
-);
-
 CREATE TABLE Ingredient (
-	idIngredient INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     strName VARCHAR(40) NOT NULL,
-    idMeasure INT NULL,
-    FOREIGN KEY FKMeasure (idMeasure) REFERENCES Measure (idMeasure)
+    measure VARCHAR(20) DEFAULT NULL
 );
 
-CREATE TABLE CocktailIngredient
-(
-	-- PRIMARY KEY (idIngredient, idCocktail),
-	idCocktailIngredient INT PRIMARY KEY AUTO_INCREMENT,
-	idIngredient INT,
-    idCocktail INT,
-    FOREIGN KEY FKCocktail (idCocktail) REFERENCES Cocktail (idCocktail),
-    FOREIGN KEY FKIngredient (idIngredient) REFERENCES Ingredient (idIngredient)
+CREATE TABLE User_Ingredient (
+	
+	id_ingredient INT NOT NULL,
+    id_User INT NOT NULL,
+    PRIMARY KEY (id_ingredient, id_User),
+    FOREIGN KEY FK_Ingredient (id_ingredient) REFERENCES Ingredient (id),
+    FOREIGN KEY FK_User (id_User) REFERENCES User (id)
 );
 
-INSERT INTO Measure (unit)
-VALUES 
-("cup"),
-("cups"),
-("oz"),
-("none"),
-("ml"),
-("tablespoon"),
-("teaspoon"),
-("teaspoons");
 
-INSERT INTO Ingredient (strName) 
-VALUES 
-("Ice"),
-("Cherry"),
-("Orange slice"),
-("Rum"),
-("Pisco"),
-("Amaretto"),
-("Whiskey"),
-("Tequila"),
-("Vodka"),
-("Grand Marnier"),
-("Coffee liqueur"),
-("Malibu rum"),
-("Gin"),
-("Rum"),
-("Cognac"),
-("Soda"),
-("Coke"),
-("Scotch"),
-("Orange juice"),
-("Pineapple juice"),
-("Lemonade"),
-("Cranberry juice"),
-("Citrus wedge"),
-("Lemon"),
-("Lime"),
-("Orange"),
-("Grapefruit"),
-("Beer"),
-("Cider"),
-("Wine"),
-("Rice wine");
-
--- One cocktail with ingredients 
-SELECT * FROM  Cocktail c 
-INNER JOIN CocktailIngredient ci ON c.idCocktail = ci.idCocktail 
-INNER JOIN Ingredient i ON i.idIngredient = ci.idIngredient
-WHERE c.idCocktail = 1;
-
--- List of cocktails with their ingredients 
-SELECT * FROM  Cocktail c 
-INNER JOIN CocktailIngredient ci ON c.idCocktail = ci.idCocktail 
-INNER JOIN Ingredient i ON i.idIngredient = ci.idIngredient;
 
 INSERT INTO user(username, password) VALUES("alex","$2b$09$mH2ysAw.MkIWxiASsWwLK.IKtVCiYZw5WyPVYdnJnRA3NpXwcGyvC");
 INSERT INTO user(username, password) VALUES("joe","$2b$09$ZyaSIfetz/wCLL/Ar/IDdOVMyQbWYp9R6OyTm3O2.QjsOJ.o6sAUG");
+
+
+-- Get all ingredients for one user 
+SELECT * FROM User u 
+INNER JOIN User_Ingredient ui ON u.id = ui.id_user 
+INNER JOIN Ingredient i ON i.id = ui.id_ingredient
+WHERE u.id = 1;
